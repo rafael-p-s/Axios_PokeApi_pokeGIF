432cdcaa3843aab37cf078ed88d75af6196b72fb
rafael-p-s <rafaelsky321@gmail.com>
2024-01-29 20:44:36 -0300

    Adicionado alterações no modo de busca e exebição dos Pokémons, acrescentando botões para avançar e carregar novos Pokémons, botão para voltar.

--- fetch_estudo/src/Pokemon.jsx
+++ fetch_estudo/src/Pokemon.jsx
@@ -5,23 +5,52 @@ import { PokeApi } from "./PokeApi";
 export function Pokemon() {
   // const url = "https://pokeapi.co/api/v2/pokemon/";
   const [list, setList] = useState([]);
-
+  const [filters, setFilters] = useState({ page: 0, resultsPerPage: 20 });
+  const [totalPokemon, setTotalPokemon] = useState(0);
   useEffect(() => {
     axios
       // .get("https://pokeapi.co/api/v2/pokemon")
-      .get("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=649/?orderByName=a")
-      .then((response) => setList(response.data.results));
+      .get(
+        `https://pokeapi.co/api/v2/pokemon?offset=${filters.page}&limit=${filters.resultsPerPage}`
+      )
+      .then((response) => {
+        setList(response.data.results);
+        setTotalPokemon(response.data.count);
+      });
 
-    
-  }, []);
-
-  
+    // fetch("https://pokeapi.co/api/v2/pokemon", {
+    //   method: "GET",
+    //   cache: "no-cache", //não está salvando nada em cache. Vantagem do FETCH
+    // })
+    //   .then((response) => response.json())
+    //   .then((response) => setList(response.results));
+  }, [filters]);
 
   return (
     <>
       {list.map((item, i) => (
         <PokeApi key={i} data={item} />
       ))}
+      <hr />
+      Total de Pg:
+      {Math.floor(totalPokemon / filters.resultsPerPage)}
+      {filters.page / 20 >= 1 && (
+        <span
+          onClick={() =>
+            setFilters((prev) => ({ ...prev, page: prev.page - 20 }))
+          }
+        >
+          {"<"}
+        </span>
+      )}
+      Pagina atual:{filters.page / 20}
+      <span
+        onClick={() =>
+          setFilters((prev) => ({ ...prev, page: prev.page + 20 }))
+        }
+      >
+        {">"}
+      </span>
     </>
   );
 }
